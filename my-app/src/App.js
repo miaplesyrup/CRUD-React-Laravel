737ed1da0785213520db2399a71870c62f1db5dd
miaplesyrup <99581223+miaplesyrup@users.noreply.github.com>
2023-12-14 19:17:27 +0800

    react side

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+import Navbar from "./components/Navbar";
+import MyRouter from "./router";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+
+      <MyRouter />
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/pages/Student.js
@@ -0,0 +1,49 @@
+import { useEffect } from "react"
+import { Link } from "react-router-dom"
+import axios from 'axios';
+
+function Student() {
+
+    useEffect(() => {
+
+        axios.get(``).then(res => {
+
+        });
+
+    }, [])
+  return (
+    <div className="container" mt-5>
+        <div className="row">
+            <div className="col-md-12">
+                <div className="card">
+                    <div className="card-header">
+                        <h4>Student Information
+                            <Link to="/" className="btn btn-primary float-end">Add Student</Link>
+                        </h4>
+                    </div>
+                    <div className="card-body">
+                        <table className="table table-striped">
+                            <thead>
+                                <tr>
+                                    <th>ID</th>
+                                    <th>Name</th>
+                                    <th>Course</th>
+                                    <th>Email</th>
+                                    <th>Phone</th>
+                                    <th>Edit</th>
+                                    <th>Delete</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Student
